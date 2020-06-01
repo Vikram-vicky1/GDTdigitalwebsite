388e054f7e59b31b1e694760fca5e5b4f4cff940
vikram <vikrammudhiraj85@gmail.com>
2020-06-01 19:47:07 +0530

    products and services header

--- js/main.js
+++ js/main.js
@@ -10,4 +10,32 @@ function myFunction() {
   }
 }
 
+jQuery('a').click( function(e) {
+  jQuery('.collapse').collapse('hide');
+});
 
+
+(function($) {
+  // DOM ready
+  $(function() {
+    // icons
+    var $icons = $('.faq-links');
+    $icons.on('click', function(e) {
+      // variables
+      var $this = $(this);
+      var $icon = $this.find('i');
+      // iconName taken from data-icon attribute
+      var iconName = $icon.data('icon');
+      // close icon
+      var closeIconName = 'fa-angle-up';
+      // prevent default browser behaviour, just in case
+      e.preventDefault();
+      // handle state
+      if ($icon.hasClass(closeIconName)) {
+        $icon.removeClass(closeIconName).addClass(iconName);
+      } else {
+        $icon.addClass(closeIconName).removeClass(iconName);
+      };
+    });
+  });
+})(jQuery);
